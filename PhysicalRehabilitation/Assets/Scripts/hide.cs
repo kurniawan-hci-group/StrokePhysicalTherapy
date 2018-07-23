e3591de0b7a36aaf9a5e8ef7098b84e85a7ef7d6
Jared Duval <ffilo26@aol.com>
2018-07-23 12:45:26 -0700

    new version

---
+++ PhysicalRehabilitation/Assets/Scripts/hide.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hide : MonoBehaviour {
+    public GameObject hideThis;
+	// Use this for initialization
+	void Start () {
+        hideThis.GetComponent<Renderer>().enabled = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
